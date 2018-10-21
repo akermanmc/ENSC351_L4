27773faadaf2aa50df0bb8e2cbf3b0e78cf99895
akermanmc <43158709+akermanmc@users.noreply.github.com>
2018-10-21 16:11:25 -0700

    in progress..

--- word_countMR_OMP.cpp
+++ word_countMR_OMP.cpp
@@ -7,29 +7,33 @@
 #include <string.h>
 #include <vector>   
 #include <algorithm>
+#include <omp.h>
 
 #define StringIntPair pair<string, int>
 using namespace std;
 
+int num_words = 0;
+
 // input function
-vector<string> inputReadFunc(vector<string> &word_vector){
+string* inputReadFunc(string wordArray[]){
 	string word;
 	ifstream inFile;
 
     inFile.open("words.txt");
     if(!inFile.is_open()){
         cout << "open failed, word_vector empty" << endl; 
-        return word_vector;
+        return wordArray;
     }
 	while (inFile >> word){
-		word_vector.push_back(word);
+		wordArray[num_words] = word;
+		num_words++;
 	}
 	inFile.close();
-	return word_vector;
+	return wordArray;
 }
 
 // map function
-StringIntPair mapFunc(const string &word){
+StringIntPair mapFunc(const string word){
     int value = 1;
     StringIntPair key_val_pair;
     key_val_pair.first = word;
@@ -38,10 +42,10 @@ StringIntPair mapFunc(const string &word){
 }
 
 //reduce function
-StringIntPair reduceFunc(vector<StringIntPair> dup_vector){
+StringIntPair reduceFunc(StringIntPair* dupArray, int dup_size){
     StringIntPair combined_pair;
-    combined_pair.first = dup_vector[0].first;
-    combined_pair.second = dup_vector.size();
+    combined_pair.first = dupArray[0].first;
+    combined_pair.second = dup_size;
     return combined_pair;
 }
 
@@ -58,21 +62,58 @@ bool compFuncDescending(const StringIntPair a, const StringIntPair b)
 }
 
 int main(){
-    vector<string> word_vector; // holds input item: vector of words parsed from input file
     vector<StringIntPair> pair_vector;  // holds initial mapped pairs
     vector<StringIntPair> dup_vector;   // holds duplicate pairs to be reduced
     vector<StringIntPair> used_vector;  // keeps track of which pairs have been tested for duplicity
     vector<StringIntPair> reduced_vector;   // holds the final reduced pairs for word_count output
+    string wordArray[1000];
+    StringIntPair pairArray[1000];
+    StringIntPair dupArray[1000];
+    StringIntPair usedArray[1000];
+    StringIntPair reducedArray[1000];
+    
+
 
     // STEP 1: read in file
-    word_vector = inputReadFunc(word_vector);
+    string* filled_wordArray = inputReadFunc(wordArray);
 
     // STEP 2: map
-    for (int i = 0; i<word_vector.size(); i++){
-        pair_vector.push_back(mapFunc(word_vector[i]));
+    #pragma omp parallel for 
+    for (int i = 0; i<num_words; i++){
+        pairArray[i] = mapFunc(wordArray[i]);
+    }
+
+    // FOR NOW: go back to vectors for reduction phase
+    for (int i = 0; i<num_words; i++){
+    	pair_vector.push_back(pairArray[i]);
     }
 
     // STEP 3: reduce: send vectors of duplicates to reduce function
+    //#pragma omp critical
+    int num_used = 0;
+    int dup_size = 0;
+    for(int i = 0; i< num_words; i++){
+    	for(int j = 0; j<num_used; j++){
+    		if (usedArray[j] == pairArray[i])
+    			continue; 
+    	}
+    	dupArray[i] = pairArray[i];
+    	dup_size++;
+    	usedArray[i] = pairArray[i];
+    	for(int k = i+1; k<num_words; k++){
+    		for (int l = 0; l<dup_size; l++){
+    			if (dupArray[l] == pairArray[k]){
+    				dupArray[k] = pairArray[k];
+    				dup_size++;
+    			}
+    		}
+    	}
+    	reducedArray[i] = reduceFunc(dupArray, dup_size);
+    	dup_size = 0;
+    }
+    cout << reducedArray[0].first << endl;
+
+/*
     for (int i= 0; i<pair_vector.size(); i++){
         if (find(used_vector.begin(), used_vector.end(), pair_vector[i]) != used_vector.end()){
             continue;
@@ -87,6 +128,7 @@ int main(){
         reduced_vector.push_back(reduceFunc(dup_vector));
         dup_vector.clear();
     }
+*/
 
     // STEP 3.5: sort the reduced vector in descending
     sort(reduced_vector.begin(), reduced_vector.end(), compFuncDescending);
